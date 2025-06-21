2c257e24d71e1a3f1fef5f9a3fbc1587db078fb6
lp64ace <akomikron@gmail.com>
2025-06-21 12:10:21 +0300

    Update import test

--- intern/mom/test/mom.cc
+++ intern/mom/test/mom.cc
@@ -6,10 +6,14 @@ namespace {
 
 #ifdef WIN32
 #	define TEST_MODULE_FILE "C:\\Windows\\System32\\kernel32.dll"
+#	define TEST_MODULE_NAME "kernel32.dll"
+#	define WIN32_LEAN_AND_MEAN
+#	include <windows.h>
 #endif
 
 #ifdef UNIX
 #	define TEST_MODULE_FILE NULL
+#	define TEST_MODULE_NAME NULL
 #endif
 
 bool mom_resolve_import(const char *libname, const char *name, int maxhops = 4) {
@@ -19,6 +23,16 @@ bool mom_resolve_import(const char *libname, const char *name, int maxhops = 4) 
 
 	ModuleHandle *handle = MOM_module_open_by_file(libname);
 	if (!handle) {
+#ifdef WIN32
+		HMODULE hmodule = LoadLibrary(libname);
+		if (!hmodule) {
+			/**
+			 * We failed to find the library though the schema table but windows cannot find it either!
+			 */
+			return true;
+		}
+		FreeLibrary(hmodule);
+#endif
 		return false;
 	}
 
@@ -54,7 +68,7 @@ bool mom_resolve_import(const char *libname, const char *name, int maxhops = 4) 
 	return found;
 }
 
-TEST(Mom, Dependency) {
+TEST(Mom, DependencyDisk) {
 	ModuleHandle *kernel32 = MOM_module_open_by_file(TEST_MODULE_FILE);
 	ASSERT_NE(kernel32, nullptr);
 	for (ModuleImport *imported = MOM_module_import_begin(kernel32); imported != MOM_module_import_end(kernel32); imported = MOM_module_import_next(kernel32, imported)) {
@@ -70,7 +84,53 @@ TEST(Mom, Dependency) {
 			}
 		}
 	}
+	for (ModuleImport *imported = MOM_module_import_delayed_begin(kernel32); imported != MOM_module_import_delayed_end(kernel32); imported = MOM_module_import_delayed_next(kernel32, imported)) {
+		const char *libname = MOM_module_import_lib(kernel32, imported);
+		if (libname) {
+			if (MOM_module_import_name(kernel32, imported)) {
+				const char *impname = MOM_module_import_name(kernel32, imported);
+				EXPECT_TRUE(mom_resolve_import(libname, impname)) << libname << " " << impname;
+			} else {
+				int impordinal = MOM_module_import_ordinal(kernel32, imported);
+				EXPECT_TRUE(mom_resolve_import(libname, reinterpret_cast<const char *>(impordinal))) << libname << " " << impordinal;
+			}
+		}
+	}
 	MOM_module_close(kernel32);
+}
+
+TEST(Mom, DependencyMemory) {
+	ProcessHandle *self = MOM_process_self();
+	do {
+		ModuleHandle *kernel32 = MOM_module_open_by_name(self, TEST_MODULE_FILE);
+		ASSERT_NE(kernel32, nullptr);
+		for (ModuleImport *imported = MOM_module_import_begin(kernel32); imported != MOM_module_import_end(kernel32); imported = MOM_module_import_next(kernel32, imported)) {
+			const char *libname = MOM_module_import_lib(kernel32, imported);
+			if (libname) {
+				if (MOM_module_import_name(kernel32, imported)) {
+					const char *impname = MOM_module_import_name(kernel32, imported);
+					EXPECT_TRUE(mom_resolve_import(libname, impname)) << libname << " " << impname;
+				} else {
+					int impordinal = MOM_module_import_ordinal(kernel32, imported);
+					EXPECT_TRUE(mom_resolve_import(libname, reinterpret_cast<const char *>(impordinal))) << libname << " " << impordinal;
+				}
+			}
+		}
+		for (ModuleImport *imported = MOM_module_import_delayed_begin(kernel32); imported != MOM_module_import_delayed_end(kernel32); imported = MOM_module_import_delayed_next(kernel32, imported)) {
+			const char *libname = MOM_module_import_lib(kernel32, imported);
+			if (libname) {
+				if (MOM_module_import_name(kernel32, imported)) {
+					const char *impname = MOM_module_import_name(kernel32, imported);
+					EXPECT_TRUE(mom_resolve_import(libname, impname)) << libname << " " << impname;
+				} else {
+					int impordinal = MOM_module_import_ordinal(kernel32, imported);
+					EXPECT_TRUE(mom_resolve_import(libname, reinterpret_cast<const char *>(impordinal))) << libname << " " << impordinal;
+				}
+			}
+		}
+		MOM_module_close(kernel32);
+	} while (false);
+	MOM_process_close(self);
 }
 
 }  // namespace
